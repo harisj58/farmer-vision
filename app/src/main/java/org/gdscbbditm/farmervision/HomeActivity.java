5e683257649d3fba22360c34792b21fefbfc0c0a
Haris Javed <harisj58@gmail.com>
2023-04-10 14:18:15 +0530

    added greeting to home screen

--- app/src/main/java/org/gdscbbditm/farmervision/HomeActivity.java
+++ app/src/main/java/org/gdscbbditm/farmervision/HomeActivity.java
@@ -6,22 +6,29 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.gdscbbditm.farmervision.R;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private Button camMode, camList, setCamName, logOut;
+    private Button camMode, camList;
     private FirebaseAuth mAuth;
     private Toolbar toolbar;
+    private TextView greeting;
+    private DatabaseReference mDbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +41,7 @@ public class HomeActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         camMode = findViewById(R.id.act1);
         camList = findViewById(R.id.act2);
-        setCamName = findViewById(R.id.act3);
-        logOut = findViewById(R.id.logout);
+        greeting = findViewById(R.id.textView3);
 
         camMode.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,28 +57,41 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-        setCamName.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(HomeActivity.this, SetName.class));
-            }
-        });
+        mDbRef = FirebaseDatabase.getInstance().getReference();
+        String uid = mAuth.getCurrentUser().getUid();
 
-        logOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mAuth.signOut();
-                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
-            }
-        });
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             String name = extras.getString("name");
             String email = extras.getString("email");
-            String uid = mAuth.getCurrentUser().getUid();
             addUserToDatabase(name, email, uid);
         }
+
+        DatabaseReference userRef = mDbRef.child("user").child(uid).child("name");
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // this method is call to get the realtime
+                // updates in the data.
+                // this method is called when the data is
+                // changed in our Firebase console.
+                // below line is for getting the data from
+                // snapshot of our database.
+                String name = snapshot.getValue(String.class);
+
+                // after getting the value we are setting
+                // our value to our text view in below line.
+                greeting.setText("Welcome, "+name+"!");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // calling on cancelled method when we receive
+                // any error or we are not able to get the data.
+                Toast.makeText(HomeActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -93,7 +112,6 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void addUserToDatabase(String name, String email, String uid) {
-        DatabaseReference mDbRef = FirebaseDatabase.getInstance().getReference();
         mDbRef.child("user").child(uid).setValue(new User(name, email, uid));
     }
 }
